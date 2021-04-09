fa56b5e1f54023da4dbe8f4e6531aef6549da6b2
vishesh-khemani <vishesh.khemani@gmail.com>
2021-04-09 10:53:00 -0700

    .

---
+++ squares/squares.js
@@ -0,0 +1,32 @@
+"use strict"
+
+function isSquare(n) {
+  let r = Math.sqrt(n);
+  return r == Math.round(r);
+}
+
+function order(orderedBeginning, unorderedEnd) {
+  if (unorderedEnd.length == 1) {
+    let first = orderedBeginning[0];
+    let last = unorderedEnd[0];
+    if (isSquare(first + last)) {
+      return orderedBeginning.concat(unorderedEnd);
+    } else {
+      return [];
+    }
+  }
+  let n = orderedBeginning[orderedBeginning.length - 1];
+  for (let i = 0; i < unorderedEnd.length; ++i) {
+    if (isSquare(n + unorderedEnd[i])) {
+      let newBeginning = [...orderedBeginning];
+      newBeginning.push(unorderedEnd[i]);
+      let newEnd = [...unorderedEnd];
+      newEnd.splice(i, 1);
+      let result = order(newBeginning, newEnd);
+      if (result.length > 0) {
+        return result;
+      }
+    }
+  }
+  return [];
+}
