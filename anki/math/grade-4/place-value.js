1951b64bcc9b9e9653da81d9cd171c3eca8e6595
vishesh-khemani <76489137+vishesh-khemani@users.noreply.github.com>
2021-05-27 07:22:53 -0700

    .

--- anki/math/grade-4/place-value.js
+++ anki/math/grade-4/place-value.js
@@ -1,1 +1,8 @@
+window.problem = {
+  a: Math.floor(Math.random() * 1_000_000_000),
+  placeStrings: ['ones', 'tens', 'hundreds', 'thousands', 'ten thousands', 'hundred thousands', 'millions', 'ten millions', 'hundred millions']
+};
+problem.place = Math.floor(Math.random() * 9);
 
+let html = `What is the digit at the ${problem.placeStrings[problem.place]} place in ${problem.a}?\n`;
+document.getElementById('front').innerHTML = html;
