0c922f00a03a7603dba4012e7c0ea8c081ae71b7
vishesh-khemani <76489137+vishesh-khemani@users.noreply.github.com>
2021-05-27 09:08:48 -0700

    .

--- anki/math/grade-4/place-value.js
+++ anki/math/grade-4/place-value.js
@@ -1,8 +1,56 @@
-window.problem = {
-  a: Math.floor(Math.random() * 1_000_000_000),
-  placeStrings: ['ones', 'tens', 'hundreds', 'thousands', 'ten thousands', 'hundred thousands', 'millions', 'ten millions', 'hundred millions']
-};
-problem.place = Math.floor(Math.random() * 9);
+function placeValue(numDigits) {
+  let placeStrings = ['ones', 'tens', 'hundreds', 'thousands', 'ten thousands',
+  'hundred thousands', 'millions', 'ten millions', 'hundred millions'];
 
-let html = `What is the digit at the ${problem.placeStrings[problem.place]} place in ${problem.a}?\n`;
-document.getElementById('body').innerHTML = html;
+  // Generate number with numDigits digits.
+  digits = [];
+  for (let i = 0; i < numDigits; ++i) {
+    let digit = Math.random();
+    if (i == 0) {
+      // [1, 9].
+      digit = Math.ceil(digit * 9);
+    } else {
+      // [0, 9]
+      digit = Math.floor(digit * 10);
+    }
+    digits.push(digit);
+  }
+  let num = 0;
+  for (let i = 0; i < numDigits; ++i) {
+    num = num + Math.pow(10, i) * digits[i];
+  }
+
+  // Generate place value to test.
+  let place = Math.floor(Math.random() * numDigits);
+
+  // Question.
+  let html = `What is the digit at the ${placeStrings[place]} place in ${num}?\n`;
+  html += `
+  <form id="answerForm">
+    <input type="text" name="answer" value=""><br>
+    <input type="submit" value="Check">
+  </form>`;
+  return [html, digits[place]];
+}
+
+function checkAnswer(correctAnswer) {
+  let guess = document.forms.answerForm.elements.answer.value;
+  let html = '';
+  if (guess == correctAnswer) {
+    html = "Correct!";
+  } else {
+    html = `Wrong. It is ${correctAnswer}`;
+  }
+  document.getElementById('checkAnswer').innerHTML = html;
+}
+
+let correctAnswer = '';
+let html = '';
+[html, correctAnswer] = placeValue(4);
+document.getElementById('front').innerHTML = html;
+let form = document.getElementById('answerForm');
+form.elements.answer.focus();
+form.onsubmit = () => {
+  event.preventDefault();
+  checkAnswer(correctAnswer);
+};
