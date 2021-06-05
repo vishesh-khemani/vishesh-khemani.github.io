e3757c172b715881e821d7ff5cd229a974d6f20a
vishesh-khemani <76489137+vishesh-khemani@users.noreply.github.com>
2021-06-05 12:33:01 -0700

    .

--- anki/math/elementary/elementary.js
+++ anki/math/elementary/elementary.js
@@ -18,141 +18,135 @@ function placeValue(numDigits) {
 }
 
 function piesFraction() {
-  // Params.
-  let numPies = randomIntInRange(2, 10);
-  let numSlicesPerPie = randomIntInRange(2, 9);
-  let numSlicesPicked = randomIntInRange(1, numPies * numSlicesPerPie + 1);
-  let answer = math.fraction(numSlicesPicked, numSlicesPerPie);
-
-  // Question.
-  let html = `There are <b>${numPies}</b> pies. Each pie is cut into ` +
-             `<b>${numSlicesPerPie}</b> equal pieces. You eat ` +
-             `<b>${numSlicesPicked}</b> pieces. How much pie did you eat ` +
-             `(as a fraction of a pie)?`;
-
-  addFormAndDisplay(html, (guess) => {
-    let isCorrect = false;
-    try {
-      let f = math.fraction(guess);
-      isCorrect = f.equals(answer);
-    } finally {
-      return [isCorrect, answer.toFraction(true)];
-    }
-  });
+  if (!areParamsDefined()) {
+    let numPies = randomIntInRange(2, 10);
+    let numSlicesPerPie = randomIntInRange(2, 9);
+    let numSlicesPicked = randomIntInRange(1, numPies * numSlicesPerPie + 1);
+    addQuestion(`There are <b>${numPies}</b> pies. Each pie is cut into ` +
+                `<b>${numSlicesPerPie}</b> equal pieces. You eat ` +
+                `<b>${numSlicesPicked}</b> pieces. How much pie did you eat ` +
+                `(as a fraction of a pie)?`);
+    let answer = math.fraction(numSlicesPicked, numSlicesPerPie);
+    addAnswer(answer.toFraction(true));
+  }
+  displayQuestion();
 }
 
 function equivalentFractions() {
-  let n1 = randomIntInRange(1, 13);
-  let d1 = randomIntInRange(1, 13);
-  let gcd = math.gcd(n1, d1);
-  if (gcd > 1) {
-    n2 = n1 / gcd;
-    d2 = d1 / gcd;
-  } else {
-    let a = randomIntInRange(2, 6);
-    n2 = n1 * a;
-    d2 = d1 * a;
-  }
-  switch (randomIntInRange(0, 4)) {
-    case 0:
+  if (!areParamsDefined()) {
+    let n1 = randomIntInRange(1, 13);
+    let d1 = randomIntInRange(1, 13);
+    let gcd = math.gcd(n1, d1);
+    if (gcd > 1) {
+      n2 = n1 / gcd;
+      d2 = d1 / gcd;
+    } else {
+      let a = randomIntInRange(2, 6);
+      n2 = n1 * a;
+      d2 = d1 * a;
+    }
+    switch (randomIntInRange(0, 4)) {
+      case 0:
       ans = n1;
       n1 = '?';
       break;
-    case 1:
+      case 1:
       ans = d1;
       d1 = '?';
       break;
-    case 2:
+      case 2:
       ans = n2;
       n2 = '?';
       break;
-    case 3:
+      case 3:
       ans = d2;
       d2 = '?';
       break;
+    }
+    addQuestion(`<b>${n1}/${d1}</b> = <b>${n2}/${d2}</b>`);
+    addAnswer(ans);
   }
-
-  let html = `<b>${n1}/${d1}</b> = <b>${n2}/${d2}</b>`;
-  addFormAndDisplay(html, (guess) => {
-    return [guess == ans, ans];
-  });
+  displayQuestion();
 }
 
 function compareFractions(sameNumerator, sameDenominator) {
-  // Params.
-  let d1 = randomIntInRange(1, 13);
-  let d2 = (sameDenominator ? d1 : randomIntInRange(1, 13));
-  let n1 = randomIntInRange(1, 13);
-  let n2 = (sameNumerator ? n1 : randomIntInRange(1, 13));
+  if (!areParamsDefined()) {
+    let d1 = randomIntInRange(1, 13);
+    let d2 = (sameDenominator ? d1 : randomIntInRange(1, 13));
+    let n1 = randomIntInRange(1, 13);
+    let n2 = (sameNumerator ? n1 : randomIntInRange(1, 13));
 
-  // Question.
-  let html = `Consider the two fractions <b>${n1}/${d1}</b> and ` +
-             `<b>${n2}/${d2}</b>. ` +
-             `Is the first fraction '>', '=', or '<' compared to the second ` +
-             `fraction?`;
+    addQuestion(`Consider the two fractions <b>${n1}/${d1}</b> and ` +
+      `<b>${n2}/${d2}</b>. Is the first fraction greater than, ` +
+      `less than or equal to the second fraction?`);
 
-  addFormAndDisplay(html, (guess) => {
-    let f1 = math.fraction(n1, d1);
-    let f2 = math.fraction(n2, d2);
-    switch (f1.compare(f2)) {
-      case 0:
-        return [guess == '=', '='];
-      case -1:
-        return [guess == '<', '<'];
-      case 1:
-        return [guess == '>', '>'];
+      let f1 = math.fraction(n1, d1);
+      let f2 = math.fraction(n2, d2);
+      let answer = '';
+      switch (f1.compare(f2)) {
+        case 0:
+          answer = 'equal';
+          break;
+        case -1:
+          answer = 'less than';
+          break;
+        case 1:
+          answer = 'greater than';
+          break;
+      }
+      addAnswer(answer);
     }
-  });
+  displayQuestion();
 }
 
 function multiplyByPowersOfTen(p) {
-  let a = randomIntInRange(1, 1_000);
-  let b = Math.pow(10, p);
-  let ans = a * b;
+  if (!areParamsDefined()) {
+    let a = randomIntInRange(1, 1_000);
+    let b = Math.pow(10, p);
+    let ans = a * b;
 
-  let html = `What is <b>${a.toLocaleString()}</b> x <b>${b}</b>?`;
-
-  addFormAndDisplay(html, (guess) => {
-    return [guess == ans, ans];
-  });
+    addQuestion(`What is <b>${a.toLocaleString()}</b> x <b>${b}</b>?`);
+    addAnswer(ans);
+  }
+  displayQuestion();
 }
 
 function divideByPowersOfTen(p) {
-  let b = Math.pow(10, p);
-  let a = randomIntInRange(1, Math.max(1_000, b * 10));
-  let rem = a % b;
-  let ans = `${Math.floor(a / b)}R${rem}`;
+  if (!areParamsDefined()) {
+    let b = Math.pow(10, p);
+    let a = randomIntInRange(1, Math.max(1_000, b * 10));
+    let rem = a % b;
+    let ans = `${Math.floor(a / b)}R${rem}`;
 
-  let html = `What is <b>${a.toLocaleString()}</b> / <b>${b}</b> ` +
-             `(in the form <i>&ltquot&gt</i>R<i>&ltrem&gt</i>)?`;
-
-  addFormAndDisplay(html, (guess) => {
-    return [guess == ans, ans];
-  });
+    addQuestion(`What is <b>${a.toLocaleString()}</b> / <b>${b}</b> ` +
+                `(in the form <i>&ltquot&gt</i>R<i>&ltrem&gt</i>)?`);
+    addAnswer(ans);
+  }
+  displayQuestion();
 }
 
 function multiplyNDigitsByMDigits(n, m) {
-  let a = randomIntWithNDigits(n);
-  let b = randomIntWithNDigits(m);
-  let ans = a * b;
+  if (!areParamsDefined()) {
+    let a = randomIntWithNDigits(n);
+    let b = randomIntWithNDigits(m);
+    let ans = a * b;
 
-  let html = `What is <b>${a.toLocaleString()}</b> x <b>${b}</b>?`;
-
-  addFormAndDisplay(html, (guess) => {
-    return [guess == ans, ans];
-  });
+    addQuestion(`What is <b>${a.toLocaleString()}</b> x <b>${b}</b>?`);
+    addAnswer(ans);
+  }
+  displayQuestion();
 }
 
 function divideNDigitsByMDigits(n, m) {
-  let a = randomIntWithNDigits(n);
-  let b = randomIntWithNDigits(m);
-  let rem = a % b;
-  let ans = `${Math.floor(a / b)}R${rem}`;
+  if (!areParamsDefined()) {
+    let a = randomIntWithNDigits(n);
+    let b = randomIntWithNDigits(m);
+    let rem = a % b;
+    let ans = `${Math.floor(a / b)}R${rem}`;
 
-  let html = `What is <b>${a.toLocaleString()}</b> / <b>${b}</b> ` +
-             `(in the form <i>&ltquot&gt</i>R<i>&ltrem&gt</i>)?`;
-
-  addFormAndDisplay(html, (guess) => {
-    return [guess == ans, ans];
-  });
+    addQuestion(`What is <b>${a.toLocaleString()}</b> / <b>${b}</b> ` +
+               `(in the form <i>&ltquot&gt</i>R<i>&ltrem&gt</i>)?`);
+    addAnswer(ans);
+  }
+  displayQuestion();
 }
--- anki/math/middle/middle.js
+++ anki/math/middle/middle.js
@@ -1,145 +1,130 @@
 function translate2D(numPoints) {
-  let by = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-  let ans = '';
-  let points = [];
-  for (let i = 0; i < numPoints; ++i) {
-    let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-    points.push(p);
-    let pImage = p.translate(by);
-    ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
-  }
-  ans = ans.trim();
+  if (!areParamsDefined()) {
+    let by = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+    let ans = '';
+    let points = [];
+    for (let i = 0; i < numPoints; ++i) {
+      let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+      points.push(p);
+      let pImage = p.translate(by);
+      ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
+    }
+    ans = ans.trim();
 
-  let html = `<div align="left";>` +
-             `Translate the following:<br>` +
-             `<ol>`;
-  for (let i = 0; i < numPoints; ++i) {
-    html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    let html = `Translate the following:<br>` +
+               `<ol>`;
+    for (let i = 0; i < numPoints; ++i) {
+      html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    }
+    html = html + `</ol>` +
+                  `By (${by.x()}, ${by.y()}).`
+    addQuestion(html);
+    addAnswer(ans);
   }
-  html = html + `</ol>` +
-                `By (${by.x()}, ${by.y()}).<br><br>` +
-                `Express the answer in the form <i>(x1, y1) (x2 y2) ...</i>` +
-                `</div>`;
-
-  addFormAndDisplay(html, (guess) => {
-    guess = guess.trim();
-    return [ans == guess, ans];
-  })
+  displayQuestion();
 }
 
 function reflectAcrossHorizontal(numPoints) {
-  let shift = randomIntInRange(-5, 6);
-  let ans = '';
-  let points = [];
-  for (let i = 0; i < numPoints; ++i) {
-    let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-    points.push(p);
-    let pImage = p.reflectAcrossHorizontal(shift);
-    ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
-  }
-  ans = ans.trim();
+  if (!areParamsDefined()) {
+    let shift = randomIntInRange(-5, 6);
+    let ans = '';
+    let points = [];
+    for (let i = 0; i < numPoints; ++i) {
+      let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+      points.push(p);
+      let pImage = p.reflectAcrossHorizontal(shift);
+      ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
+    }
+    ans = ans.trim();
 
-  let html = `<div align="left";>` +
-             `Reflect the following:<br>` +
-             `<ol>`;
-  for (let i = 0; i < numPoints; ++i) {
-    html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    let html = `Reflect the following:<br>` +
+               `<ol>`;
+    for (let i = 0; i < numPoints; ++i) {
+      html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    }
+    html = html + `</ol>` +
+                  `Across the line <i>y = ${shift}</i>.`;
+    addQuestion(html);
+    addAnswer();
   }
-  html = html + `</ol>` +
-                `Across the line <i>y = ${shift}</i>.<br><br>` +
-                `Express the answer in the form <i>(x1, y1) (x2 y2) ...</i>` +
-                `</div>`;
-
-  addFormAndDisplay(html, (guess) => {
-    guess = guess.trim();
-    return [ans == guess, ans];
-  })
+  displayQuestion(ans);
 }
 
 function reflectAcrossVertical(numPoints) {
-  let shift = randomIntInRange(-5, 6);
-  let ans = '';
-  let points = [];
-  for (let i = 0; i < numPoints; ++i) {
-    let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-    points.push(p);
-    let pImage = p.reflectAcrossVertical(shift);
-    ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
-  }
-  ans = ans.trim();
+  if (!areParamsDefined()) {
+    let shift = randomIntInRange(-5, 6);
+    let ans = '';
+    let points = [];
+    for (let i = 0; i < numPoints; ++i) {
+      let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+      points.push(p);
+      let pImage = p.reflectAcrossVertical(shift);
+      ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
+    }
+    ans = ans.trim();
 
-  let html = `<div align="left";>` +
-             `Reflect the following:<br>` +
-             `<ol>`;
-  for (let i = 0; i < numPoints; ++i) {
-    html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    let html = `Reflect the following:<br>` +
+               `<ol>`;
+    for (let i = 0; i < numPoints; ++i) {
+      html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    }
+    html = html + `</ol>` +
+                  `Across the line <i>x = ${shift}</i>.`;
+    addQuestion(html);
+    addAnswer(ans);
   }
-  html = html + `</ol>` +
-                `Across the line <i>x = ${shift}</i>.<br><br>` +
-                `Express the answer in the form <i>(x1, y1) (x2 y2) ...</i>` +
-                `</div>`;
-
-  addFormAndDisplay(html, (guess) => {
-    guess = guess.trim();
-    return [ans == guess, ans];
-  })
+  displayQuestion();
 }
 
 function rotateAroundOrigin(numPoints) {
-  let num90Counter = randomIntInRange(1, 4);
-  let ans = '';
-  let points = [];
-  for (let i = 0; i < numPoints; ++i) {
-    let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-    points.push(p);
-    let pImage = p.rotateAroundOrigin(num90Counter);
-    ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
-  }
-  ans = ans.trim();
+  if (!areParamsDefined()) {
+    let num90Counter = randomIntInRange(1, 4);
+    let ans = '';
+    let points = [];
+    for (let i = 0; i < numPoints; ++i) {
+      let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+      points.push(p);
+      let pImage = p.rotateAroundOrigin(num90Counter);
+      ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
+    }
+    ans = ans.trim();
 
-  let html = `<div align="left";>` +
-             `Rotate the following:<br>` +
-             `<ol>`;
-  for (let i = 0; i < numPoints; ++i) {
-    html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    let html = `Rotate the following:<br>` +
+               `<ol>`;
+    for (let i = 0; i < numPoints; ++i) {
+      html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    }
+    html = html + `</ol>` +
+                  `Counterclockwise about the origin by ` +
+                  `<i>${num90Counter * 90} degrees</i>.`;
+    addQuestion(html);
+    addAnswer(ans);
   }
-  html = html + `</ol>` +
-                `Counterclockwise about the origin by ` +
-                `<i>${num90Counter * 90} degrees</i>.<br><br>` +
-                `Express the answer in the form <i>(x1, y1) (x2 y2) ...</i>` +
-                `</div>`;
-
-  addFormAndDisplay(html, (guess) => {
-    guess = guess.trim();
-    return [ans == guess, ans];
-  })
+  displayQuestion();
 }
 
 function dilateAtOrigin(numPoints) {
-  let scale = (Math.random() < 0.5 ? randomIntInRange(2, 5) : 0.5);
-  let ans = '';
-  let points = [];
-  for (let i = 0; i < numPoints; ++i) {
-    let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
-    points.push(p);
-    let pImage = p.dilateAtOrigin(scale);
-    ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
-  }
-  ans = ans.trim();
+  if (!areParamsDefined()) {
+    let scale = (Math.random() < 0.5 ? randomIntInRange(2, 5) : 0.5);
+    let ans = '';
+    let points = [];
+    for (let i = 0; i < numPoints; ++i) {
+      let p = new Point(randomIntInRange(-5, 6), randomIntInRange(-5, 6));
+      points.push(p);
+      let pImage = p.dilateAtOrigin(scale);
+      ans = ans + `(${pImage.x()}, ${pImage.y()}) `;
+    }
+    ans = ans.trim();
 
-  let html = `<div align="left";>` +
-             `Dilate the following:<br>` +
-             `<ol>`;
-  for (let i = 0; i < numPoints; ++i) {
-    html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    let html = `Dilate the following:<br>` +
+               `<ol>`;
+    for (let i = 0; i < numPoints; ++i) {
+      html = html + `<li>(${points[i].x()}, ${points[i].y()})</li> `
+    }
+    html = html + `</ol>` +
+                  `By <i>${scale}</i>, centered at the origin.`;
+    addQuestion(html);
+    addAnswer(ans);
   }
-  html = html + `</ol>` +
-                `By <i>${scale}</i>, centered at the origin.<br><br>` +
-                `Express the answer in the form <i>(x1, y1) (x2 y2) ...</i>` +
-                `</div>`;
-
-  addFormAndDisplay(html, (guess) => {
-    guess = guess.trim();
-    return [ans == guess, ans];
-  })
+  displayQuestion();
 }
